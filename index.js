542a66369b55e85b9fdd73ed459f10fb1ee51c54
caymankun <111965236+caymankun@users.noreply.github.com>
2023-03-31 22:54:16 +0900

    Add files via upload

---
+++ index.js
@@ -0,0 +1,33 @@
+const ham = $('#js-hamburger');
+const nav = $('#js-nav');
+ham.on('click', function () { //ハンバーガーメニューをクリックしたら
+  ham.toggleClass('active'); // ハンバーガーメニューにactiveクラスを付け外し
+  nav.toggleClass('active'); // ナビゲーションメニューにactiveクラスを付け外し
+
+});
+
+$(function(){
+ 
+ //音を鳴らす
+ $('.headhn').mouseover(function(){
+
+     document.getElementById("overSound").currentTime = 0;
+     document.getElementById("overSound").play();
+
+ });
+
+
+});
+
+
+
+function foo() {
+    if (document.getElementById("check").checked) {
+      document.removeEventListener('touchmove', noscroll);
+    document.removeEventListener('wheel', noscroll);
+      window.scrollTo(0, 0);
+    } else {
+      document.addEventListener('touchmove', noscroll, {passive: false});
+    document.addEventListener('wheel', noscroll, {passive: false});
+  }
+}
